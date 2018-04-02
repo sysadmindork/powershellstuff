8408cf9accbb61764f291ee47be480887effc8e3
Tyler Helwig <helwigdev@gmail.com>
2018-04-02 13:41:35 -0400

    include module dependency checks

--- ExchangeOnline.ps1
+++ ExchangeOnline.ps1
@@ -1,6 +1,25 @@
 #  Exchange Online Powershell Hooker-upper
 # Requires msonline, azuread
 
+#function to check if a given module is installed and available and import if so
+function CheckModuleAvailable ($modulename) {
+    if (Get-Module -ListAvailable -Name $modulename) {
+        Write-Host "$modulename module available - proceeding"
+        Import-Module $modulename
+        return $true
+    } else {
+        Write-Error "$modulename module not available"
+        return $false
+    }
+}
+
+#check dependencies
+$ms = CheckModuleAvailable("MSOnline")
+$azure = CheckModuleAvailable("AzureAD")
+if(-not ($ms -and $azure)){
+    exit
+}
+
 # Get creds from the user
 
 $UserCredential = Get-Credential
